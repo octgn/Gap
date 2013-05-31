8e8a0c7934ab8be883875237265da61c51185495
Kelly Elton <kelly.elton@skylabsonline.com>
2013-05-31 15:04:02 -0500

    More goods.

---
+++ Gap/PythonEngine.cs
@@ -0,0 +1,33 @@
+﻿namespace Gap
+{
+    using IronPython.Hosting;
+
+    using Microsoft.Scripting.Hosting;
+
+    public class PythonEngine
+    {
+        #region PythonEngineSingleton
+
+        internal static PythonEngine Context { get; set; }
+
+        private static readonly object PythonEngineSingletonLocker = new object();
+
+        public static PythonEngine Get()
+        {
+            lock (PythonEngineSingletonLocker) return Context ?? (Context = new PythonEngine());
+        }
+
+        internal PythonEngine()
+        {
+            Engine = Python.CreateEngine();
+            Engine.CreateScope();
+        }
+
+        #endregion Singleton
+
+        internal ScriptEngine Engine { get; set; }
+        internal ScriptScope Scope { get; set; }
+
+        
+    }
+}
