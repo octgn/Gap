30eb76a9308a8c4e2af4f1a06e436e7c6b1c3467
Kelly Elton <kelly.elton@skylabsonline.com>
2014-05-30 11:12:55 -0500

    Fixed all channels sending to lobby. Added logentries

--- Gap/IrcBot.cs
+++ Gap/IrcBot.cs
@@ -117,6 +117,7 @@
         {
             Log.Info(args.Sender.Username.ToString() + ":" + args.Message.ToString());
             if (args.Message.StartsWith("@")) return;
+            if (args.Recipient.StartsWith("#") && args.Recipient.Equals("#octgn-lobby") == false) return;
             MessageQueue.Get().Add(new MessageItem(args.Sender.Nickname, args.Message, Destination.Xmpp));
         }
 
