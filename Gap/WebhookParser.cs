22e7186ecb5645b3b72065323a2621d2566d2ae6
Uri <sakura256@gmail.com>
2015-05-29 23:09:57 +0200

    Removed old hooks added release event

--- Gap/WebhookParser.cs
+++ Gap/WebhookParser.cs
@@ -67,212 +67,6 @@ namespace Gap
             return ret;
         }
 
-        protected string DoParse2(WebhookQueueMessage message)
-        {
-            dynamic d = JsonConvert.DeserializeObject(message.Body);
-
-            string ghmessage = null;
-            var action = (string)d.action;
-            if (action != null)
-            {
-                switch (action)
-                {
-                    case "created":
-                        ghmessage = string.Format("[{0}] {1} commented on the issue #{2}: {3} - {4}",
-                            d.repository.name, d.sender.login, d.issue.number, d.issue.title,
-                            d.issue.html_url);
-                        break;
-                    case "reopened":
-                        if (d.issue != null)
-                        {
-                            ghmessage = string.Format("[{0}] {1} reopened issue #{2}: {3} - {4}",
-                                d.repository.name, d.sender.login, d.issue.number, d.issue.title,
-                                d.issue.html_url);
-                        }
-                        else if (d.pull_request != null)
-                        {
-                            ghmessage = string.Format("[{0}] {1} reopened pull request #{2}: {3} - {4}",
-                                d.repository.name, d.sender.login, d.pull_request.number, d.pull_request.title,
-                                d.pull_request.html_url);
-                        }
-                        break;
-                    case "opened":
-                        if (d.issue != null)
-                        {
-                            ghmessage = string.Format("[{0}] {1} opened issue #{2}: {3} - {4}",
-                                d.repository.name, d.sender.login, d.issue.number, d.issue.title,
-                                d.issue.html_url);
-                            ;
-                        }
-                        else if (d.pull_request != null)
-                        {
-                            ghmessage = string.Format("[{0}] {1} opened pull request #{2}: {3} - {4}",
-                                d.repository.name, d.sender.login, d.pull_request.number, d.pull_request.title,
-                                d.pull_request.html_url);
-                        }
-                        break;
-                    case "closed":
-                        if (d.issue != null)
-                        {
-                            ghmessage = string.Format("[{0}] {1} closed issue #{2}: {3} - {4}",
-                                d.repository.name, d.sender.login, d.issue.number, d.issue.title,
-                                d.issue.html_url);
-                        }
-                        else if (d.pull_request != null)
-                        {
-                            ghmessage = string.Format("[{0}] {1} closed pull request #{2}: {3} - {4}",
-                                d.repository.name, d.sender.login, d.pull_request.number, d.pull_request.title,
-                                d.pull_request.html_url);
-                        }
-                        break;
-                    case "started":
-                        ghmessage = string.Format("[{0}] {1} starred repository", d.repository.name, d.sender.login);
-                        break;
-                    case "labeled":
-                        ghmessage = string.Format("[{0}] {1} added label {2} to issue #{3} - {4}", d.repository.name, d.sender.login, d.label.name, d.issue.number, d.issue.html_url);
-                        break;
-                    case "unlabeled":
-                        ghmessage = string.Format("[{0}] {1} removed label {2} from issue #{3} - {4}", d.repository.name, d.sender.login, d.label.name, d.issue.number, d.issue.html_url);
-                        break;
-                    case "assigned":
-                        ghmessage = string.Format("[{0}] {1} assigned {2} to issue #{3} - {4}", d.repository.name, d.sender.login, d.assignee.login, d.issue.number, d.issue.html_url);
-                        break;
-                    case "unassigned":
-                        ghmessage = string.Format("[{0}] {1} removed {2} from issue #{3} - {4}", d.repository.name, d.sender.login, d.assignee.login, d.issue.number, d.issue.html_url);
-                        break;
-                    case "synchronize":
-                        {
-                            ghmessage = string.Format("[{0}] {1} updated pull request #{2}: {3} - {4}",
-                                    d.repository.name, d.sender.login, d.pull_request.number, d.pull_request.title,
-                                    d.pull_request.html_url);
-                            break;
-                        }
-                }
-            }
-            else if (d.@ref != null && d.deleted != null && (bool)d.deleted)
-            {
-                ghmessage = string.Format("[{0}] {1} deleted the branch {2}", d.repository.name, d.pusher.name, d.@ref);
-            }
-            else if (d.commits != null)
-            {
-                if ((d.commits as JArray).Count == 0)
-                {
-                    if (d.head_commit != null)
-                        d.commits = new JArray(d.head_commit);
-                    else
-                        return null;
-                }
-                var messages = new List<string>();
-                foreach (var com in d.commits)
-                {
-                    var title = "";
-                    var desc = "";
-                    using (var sr = new StringReader((string)com.message))
-                    {
-                        title += sr.ReadLine();
-                        sr.ReadLine();
-                        var line = sr.ReadLine();
-                        while (line != null)
-                        {
-                            desc += line;
-                            line = sr.ReadLine();
-                        }
-                    }
-                    messages.Add(string.Format("[{0}] {1} made a commit {2} -> {3} {4}", d.repository.name, com.author.name, title, desc, com.url));
-                }
-                ghmessage = string.Join("\n", messages);
-            }
-            else if (d.pages != null)
-            {
-                var messages = new List<string>();
-                foreach (var p in d.pages)
-                {
-                    var sum = " ";
-                    if (p.summary != null)
-                    {
-                        sum = " \"" + p.summary + "\" ";
-                    }
-                    //                       [repo] user action page_name summary url
-                    var mess = string.Format("[{0}] {1} {2} {3}{4}{5}",
-                        d.repository.name, d.sender.login, p.action, p.page_name, sum,
-                        p.html_url);
-                    messages.Add(mess);
-                }
-                ghmessage = string.Join("\n", messages);
-            }
-            else if (d.context != null)
-            {
-                var context = (string)d.context;
-                if (context.StartsWith("continuous-integration"))
-                {
-                    ghmessage = string.Format("[{0}] {1} {2}", d.context, d.description, d.target_url);
-                }
-                else if (context.StartsWith("clahub"))
-                {
-                    if (d.state != null)
-                    {
-                        if (d.state == "failure")
-                        {
-                            if (d.description == "Not all contributors have signed the Contributor License Agreement.")
-                            {
-                                ghmessage = string.Format("[OCTGN] {0} {1}", d.description, d.target_url);
-                            }
-                            else if (d.description == "One or more of this commit's parents has contributors who have not signed the Contributor License Agreement.")
-                            {
-                                ghmessage = string.Format("[OCTGN] {0} commited {1}, but still hasn't signed the CLA {2}", d.commit.commit.author.name, d.commit.html_url, d.target_url);
-                            }
-                        }
-                        else if (d.state == "success")
-                        {
-                            if (d.description == "All contributors have signed the Contributor License Agreement.")
-                            {
-                                ghmessage = "IGNORE";
-                            }
-                        }
-                    }
-                }
-                else if (context.StartsWith("Build"))
-                {
-                    if (d.description.ToString().Trim().StartsWith("Build started"))
-                    {
-                        ghmessage = string.Format("[OCTGN] Build started for {0}", d.commit.html_url);
-                    }
-                    else if (d.description == "Build finished.")
-                    {
-                        ghmessage = string.Format("[OCTGN] Build {0} for {1}", d.state, d.commit.html_url);
-                    }
-                    else if (d.description.ToString().Trim().StartsWith("Build triggered"))
-                    {
-                        ghmessage = string.Format("[OCTGN] Build triggered for {0}", d.commit.html_url);
-                    }
-                }
-                else if (context == "OCTGN-PRTester")
-                {
-                    ghmessage = string.Format("[OCTGN] " + d.description);
-                }
-            }
-            else if (d.ref_type != null && d.@ref != null)
-            {
-                if (d.ref_type == "tag")
-                {
-                    ghmessage = string.Format("[{0}] {1} created the tag {2}", d.repository.name, d.sender.login, d.@ref);
-                }
-                else if (d.ref_type == "branch")
-                {
-                    ghmessage = string.Format("[{0}] {1} created the branch {2}", d.repository.name, d.sender.login, d.@ref);
-                }
-            }
-            else if (d.forkee != null)
-            {
-                ghmessage = string.Format("[{0}] {1} forked repository {2}", d.repository.name, d.forkee.owner.login, d.forkee.html_url);
-            }
-            else if (d.team != null)
-            {
-                ghmessage = string.Format("[OCTGN] Team {0} created", d.team.name);
-            }
-            return ghmessage;
-        }
-
         protected override string DoParse(WebhookQueueMessage message)
         {
             string ghMessage = null;
@@ -287,7 +81,7 @@ namespace Gap
             }
             if (eventType == null)
             {
-                ghMessage = DoParse2(message);
+                return (ghMessage);
             }
 
             dynamic d = JsonConvert.DeserializeObject(message.Body);
@@ -397,6 +191,9 @@ namespace Gap
                         commitMessages.Add(string.Format("[{0}] {1} made a commit {2} -> {3} {4}", d.repository.name, com.author.name, title, desc, com.url));
                     }
                     ghMessage = string.Join("\n", commitMessages);
+                    break;
+                case "release":
+                    ghMessage = string.Format("[{0}] {1} added release: {2} - {3}", d.repository.name, d.sender.login, d.release.name, d.release.html_url);
                     break;
                 case "status":
                     ghMessage = "IGNORE";
