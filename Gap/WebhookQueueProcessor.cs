65cf0910e52dedb7673bb6b88162fff6550fad83
Kelly Elton <its.the.doc@gmail.com>
2016-09-27 02:28:46 -0400

    Use Azure for webhook shit

--- Gap/WebhookQueueProcessor.cs
+++ Gap/WebhookQueueProcessor.cs
@@ -1,123 +1,85 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.Caching;
-using System.Timers;
-using Amazon.CloudFront.Model;
-using Amazon.SQS;
-using Amazon.SQS.Model;
-using IronPython.Modules;
 using log4net;
-using Newtonsoft.Json;
 using Octgn.Site.Api.Models;
+using System.Configuration;
+using Microsoft.ServiceBus.Messaging;
 
 namespace Gap
 {
     public class WebhookQueueProcessor : IDisposable
     {
         internal static ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-        private readonly Timer _processHooksTimer;
-        private readonly MemoryCache _processedMessages = new MemoryCache("ProcessedMessages");
+        private QueueClient _client;
 
-        public WebhookQueueProcessor()
-        {
-            _processHooksTimer = new Timer(2000);
-            _processHooksTimer.Elapsed += ProcessHooksTimerOnElapsed;
+        public WebhookQueueProcessor() {
         }
 
-        public void Start()
-        {
+        public void Start() {
             Log.Info("Starting WebhookQueueProcessor");
-            _processHooksTimer.Start();
+            var constring = ConfigurationManager.AppSettings["AzureServiceBusWebhookQueue"];
+            Log.Info("Connection String: " + constring);
+            _client = QueueClient.CreateFromConnectionString(constring);
+            var opts = new OnMessageOptions() {
+                AutoComplete = false,
+            };
+            _client.OnMessage(ProcessMessage, opts);
         }
 
-        private void ProcessHooksTimerOnElapsed(object sender, ElapsedEventArgs elapsedEventArgs)
-        {
-            try
-            {
-                _processHooksTimer.Enabled = false;
-                using (var client = new AmazonSQSClient())
-                {
-                    var req = new ReceiveMessageRequest();
-                    req.MaxNumberOfMessages = 5;
-                    req.QueueUrl = "https://sqs.us-east-1.amazonaws.com/194315037322/WebhookQueue";
-
-                    var resp = client.ReceiveMessage(req);
-
-                    foreach (var m in resp.Messages)
-                    {
-                        if (_processedMessages.Contains(m.MessageId))
-                            continue;
-                        _processedMessages.Add(m.MessageId, m.MessageId, DateTimeOffset.Now.AddHours(1));
-                        var mess = JsonConvert.DeserializeObject<WebhookQueueMessage>(m.Body);
-
-                        var endmessage = WebhookParser.Parse(mess);
-                        var parsed = true;
-                        if (endmessage == null)
-                        {
-                            var parser = WebhookParser.Get(mess);
-                            if (parser == null)
-                            {
-                                Log.Error("Could not find parser for message\n" + mess.Body);
-                                endmessage = "Could not find parser for message";
-                            }
-                            else
-                            {
-                                Log.Error(parser.GetType().Name + " failed to parse\n" + mess.Body);
-                                if (parser.GetType() == typeof (GithubWebhookParser))
-                                {
-                                    endmessage = parser.GetType().Name + " failed to parse message of event type: " +
-                                                 mess.Headers["X-GitHub-Event"].First();
-                                }
-                                else
-                                {
-                                    endmessage = parser.GetType().Name + " failed to parse message";
-                                }
-                            }
-                            parsed = false;
-                        }
-                        if (endmessage != "IGNORE")
-                        {
-                            switch (mess.Endpoint)
-                            {
-                                case WebhookEndpoint.Octgn:
-                                    MessageQueue.Get().Add(new MessageItem("Cpt. Hook", endmessage, Destination.IrcOctgn));
-                                    break;
-                                case WebhookEndpoint.OctgnDev:
-                                    MessageQueue.Get().Add(new MessageItem("Cpt. Hook", endmessage, Destination.IrcOctgnDev));
-                                    break;
-                                case WebhookEndpoint.OctgnLobby:
-                                    MessageQueue.Get().Add(new MessageItem("Cpt. Hook", endmessage, Destination.IrcOctgnLobby));
-                                    break;
-                                default:
-                                    throw new ArgumentOutOfRangeException(mess.Endpoint.ToString());
+        private void ProcessMessage(BrokeredMessage bmess) {
+            try {
+                using (bmess) {
+                    var message = bmess.GetBody<WebhookQueueMessage>();
+                    var endmessage = WebhookParser.Parse(message);
+                    var parsed = true;
+                    if (endmessage == null) {
+                        var parser = WebhookParser.Get(message);
+                        if (parser == null) {
+                            Log.Error("Could not find parser for message\n" + message.Body);
+                            endmessage = "Could not find parser for message";
+                        } else {
+                            Log.Error(parser.GetType().Name + " failed to parse\n" + message.Body);
+                            if (parser.GetType() == typeof(GithubWebhookParser)) {
+                                endmessage = parser.GetType().Name + " failed to parse message of event type: " +
+                                             message.Headers["X-GitHub-Event"].First();
+                            } else {
+                                endmessage = parser.GetType().Name + " failed to parse message";
                             }
                         }
-
-                        if (Program.IrcBot.IrcClient.IsConnected && parsed)
-                        {
-                            var req2 = new DeleteMessageRequest();
-                            req2.QueueUrl = req.QueueUrl;
-                            req2.ReceiptHandle = m.ReceiptHandle;
-                            client.DeleteMessage(req2);
+                        parsed = false;
+                    }
+                    if (endmessage != "IGNORE") {
+                        switch (message.Endpoint) {
+                            case WebhookEndpoint.Octgn:
+                            MessageQueue.Get().Add(new MessageItem("Cpt. Hook", endmessage, Destination.IrcOctgn));
+                            break;
+                            case WebhookEndpoint.OctgnDev:
+                            MessageQueue.Get().Add(new MessageItem("Cpt. Hook", endmessage, Destination.IrcOctgnDev));
+                            break;
+                            case WebhookEndpoint.OctgnLobby:
+                            MessageQueue.Get().Add(new MessageItem("Cpt. Hook", endmessage, Destination.IrcOctgnLobby));
+                            break;
+                            default:
+                            throw new ArgumentOutOfRangeException(message.Endpoint.ToString());
                         }
                     }
+
+                    if (Program.IrcBot.IrcClient.IsConnected && parsed) {
+                        bmess.Complete();
+                    }
                 }
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 Log.Error("ProcessHooksTimerOnElapsed", e);
-            }
-            finally
-            {
-                _processHooksTimer.Enabled = true;
             }
         }
 
-        public void Dispose()
-        {
-            _processHooksTimer.Elapsed -= ProcessHooksTimerOnElapsed;
+        public void Dispose() {
+            try {
+                _client?.Close();
+                _client = null;
+            } catch { }
         }
     }
 }
